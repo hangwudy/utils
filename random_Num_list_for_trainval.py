7c338605189b88b892dbc99452f213c7c9aad335
hangwudy <hang.wu@yahoo.com>
2018-10-17 11:35:13 +0200

    add new features

--- random_Num_list_for_trainval.py
+++ random_Num_list_for_trainval.py
@@ -3,9 +3,13 @@
 
 import random
 
-Lt = random.sample(range(1, 201), 200)
-file = open('trainval.txt', 'w')
-for i in range(200):
-    file.write('NAME-{}\n'.format(Lt[i]))
-    print('NAME-{}'.format(Lt[i]))
-file.close()
+
+def list_output():
+    Lt = random.sample(range(1, 201), 200)
+    file = open('trainval.txt', 'w')
+    for i in range(200):
+        file.write('NAME-{}\n'.format(Lt[i]))
+        print('NAME-{}'.format(Lt[i]))
+    file.close()
+
+
