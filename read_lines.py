4cfc74b5df33a0bb042211b22118283a97c79090
hangwudy <hangwudy@users.noreply.github.com>
2019-02-14 15:14:33 +0100

    update

---
+++ read_lines.py
@@ -0,0 +1,88 @@
+# read file script
+# Created by Hang Wu
+# Data: 2019.02.14
+
+
+import os
+import xlsxwriter
+
+
+def read_lines(file_path):
+    with open(file_path) as f:
+        # counter = 0
+        Robot_Coor = []
+        output = []
+        for line in f:
+            # delete the space in the head & end of the line
+            line = line.strip()
+            # determine wether the line is space or comment
+            if not len(line) or line.startswith("#"):
+                continue
+            if "Actual" in line:
+                continue
+            # if "EXTAXS" in line:
+            #     counter += 1
+            # if "EXTAXS" in line and counter > 1:
+            #     continue
+            if "EXTAXS" in line:
+                door_longtitude = line.split(" ")[-1]
+                continue
+            if "TCP" in line:
+                continue
+
+            value = line.split(" ")[-1]
+            Robot_Coor.append(value)
+        output.append(door_longtitude)
+        output = output + Robot_Coor
+
+        # for i in output:
+        #     print(i)
+        return output
+
+
+def get_file_path(file_dir, ext="txt", keyword="Robot"):
+    file_list = []
+    for filename in os.listdir(file_dir):
+        if filename.endswith(ext) and filename.find(keyword) != -1:
+            current_path = os.path.abspath(file_dir)
+            file_abs_path = os.path.join(current_path, filename)
+            file_list.append(file_abs_path)
+    return file_list
+
+
+if __name__ == "__main__":
+
+    file_path = "C:\\Users\\magic\\Documents\\Messungen\\2019-02-13_14-40-26_Robot_Trafo.txt"
+    file_dir = "C:\\Users\\magic\\Documents\\Messungen"
+    file_output = "C:\\Users\\magic\\Dropbox\\Masterarbeit\\Messungen.txt"
+    xlsx_file = "C:\\Users\\magic\\Dropbox\\Masterarbeit\\Messungen.xlsx"
+
+    workbook = xlsxwriter.Workbook(xlsx_file)
+    worksheet = workbook.add_worksheet()
+
+    # out = read_lines(file_path)
+    # print(out)
+
+    file_list = get_file_path(file_dir)
+    counter = 0
+    with open(file_output, "w") as file_out:
+        col = 10
+        for file_txt in file_list:
+            row = 10
+            file_name = os.path.split(file_txt)[-1][:-4]
+            Door_Robot_Coor = read_lines(file_txt)
+            counter += 1
+            file_out.write("======================== {} ========================\n".format(counter))
+            # file_out.write(file_name+"\n")
+            file_out.write("================ {} ================\n".format(file_name))
+            file_out.write(file_name + "\n")
+            # Excel
+            worksheet.write(row, col, file_name)
+            for i in Door_Robot_Coor:
+                file_out.write(i + "\n")
+                # Excel
+                worksheet.write(row + 1, col, i)
+                row += 1
+            col += 1
+            file_out.write("========================================================\n")
+    workbook.close()
---
+++ utils.py
@@ -0,0 +1,83 @@
+
+# coding: utf-8
+import re
+import random
+import os
+
+
+def random_list(NAME):
+    x_angle = random.sample(range(0, 50), 50)
+    y_angle = random.sample(range(0,360),360)
+    name_list = []
+    for i in x_angle:
+        for j in y_angle:
+            name_list.append('{}_{}_{}\n'.format(NAME, i, j))
+    
+    random.shuffle(name_list)
+
+# output a txt file
+def random_list_file_export(out_list):
+    file = open('trainval.txt', 'w')
+    for row in range(len(out_list)):
+        file.write(out_list[row]+'\n')
+    file.close()
+
+
+def get_class_name_from_filename(file_name):
+    """Gets the class name from a file.
+
+    Args:
+      file_name: The file name to get the class name from.
+                ie. "american_pit_bull_terrier_20_105.jpg"
+
+    Returns:
+      A string of the class name.
+    """
+    match = re.match(r'([A-Za-z_]+)(_+)([0-9]+)(_+)([0-9]+)(\.jpg)', file_name, re.I)
+    print(match.groups()[0])
+    print(match.groups()[2])
+    print(match.groups()[4])
+    return match.groups()[0]
+
+# get image absolut path
+def loadim(image_path = 'images', ext = 'png', key_word = 'car_door'):
+    image_list = []
+    for filename in os.listdir(image_path):
+        if filename.endswith(ext) and filename.find(key_word) != -1:
+            current_path = os.path.abspath(image_path)
+            image_abs_path = os.path.join(current_path,filename)
+            image_list.append(image_abs_path)
+    return image_list
+
+# Wrong !!!
+def list_shuffle(list_to_shuffle):
+    # Wrong !!!! 
+    print(random.shuffle(list_to_shuffle))
+    return random.shuffle(list_to_shuffle)
+
+# from absolut path to file name
+def file_name_from_path(file_list):
+    file_name_list = []
+    for file_path in file_list:
+        fname = file_path.split(os.path.sep)[-1]
+        fname = fname.split('.')[-2]
+        file_name_list.append(fname)
+    
+    return file_name_list
+
+
+
+if __name__ == '__main__':
+    # random_list('car_door')
+    # get_class_name_from_filename('car_door_1_2.jpg')
+    raw_list = loadim('/home/hangwu/CyMePro/data/images','jpg','car_door')
+    # print(raw_list)
+    # shuffle_list = list_shuffle(raw_list)
+    name_list = file_name_from_path(raw_list)
+    print(name_list)
+    random.shuffle(name_list)
+    print(name_list)
+    random_list_file_export(name_list)
+
+
+
